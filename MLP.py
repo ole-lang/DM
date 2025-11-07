37556590eeae543139baca5c0aa74748183543a5
NorDih <157356744+NorDih@users.noreply.github.com>
2025-11-07 16:58:39 +0100

    added MLP

--- MLP.py
+++ MLP.py
@@ -1,2 +1,67 @@
 # create a mullti-layer perceptron model for fuel consumption prediction
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
+import tensorflow as tf
+import keras
+from keras import layers
+
+from Data import fuel_intervals as data
+
+# Drop NaN rows
+# environment variable: TF_ENABLE_ONEDNN_OPTS=0
+df_features = data.dropna()
+
+# Define features and target
+X = df_features.drop(columns=["fuel_diff_ml", "start_time", "end_time"], errors='ignore')
+y = df_features["fuel_diff_ml"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normalize features (important for neural networks)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Define model
+# Define model
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(X_train_scaled.shape[1],)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(16, activation='relu'),
+    layers.Dense(1)  # output = predicted fuel_used
+])
+# Compile model
+model.compile(
+    optimizer='adam',
+    loss='mean_squared_error',
+    metrics=['mae']
+)
+
+model.summary()
+
+history = model.fit(
+    X_train_scaled, y_train,
+    validation_split=0.2,
+    epochs=100,
+    batch_size=16,
+    verbose=1
+)
+
+test_loss, test_mae = model.evaluate(X_test_scaled, y_test)
+
+# Predictions
+y_pred = model.predict(X_test_scaled).flatten()
+print(f"Test MAE: {test_mae:.4f}")
+print(f"Test Loss (MSE): {test_loss:.4f}")
+# print(f"R2 Score: {keras.metrics.R2Score()(y_test, y_pred).numpy():.4f}")
+
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual fuel used")
+plt.ylabel("Predicted fuel used")
+plt.title("MLP predictions vs actual")
+plt.show()
--- Random_Forest.py
+++ Random_Forest.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
 
 from Data import fuel_intervals as data
 
@@ -26,3 +27,14 @@ y_pred = model.predict(X_test)
 print(f"R² = {r2_score(y_test, y_pred):.3f}")
 print(f"MAE = {mean_absolute_error(y_test, y_pred):.3f} liters")
 print(f"RMSE = {np.sqrt(mean_squared_error(y_test, y_pred)):.3f} liters")
+
+# simple Scatter-Plot
+plt.figure(figsize=(6,6))
+plt.scatter(y_test, y_pred, alpha=0.7)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color="k", linestyle="--")
+plt.xlabel("Actual fuel_diff_ml")
+plt.ylabel("Predicted fuel_diff_ml")
+plt.title("Predicted vs. Actual")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
